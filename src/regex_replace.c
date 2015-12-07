8e9b270c3425eefbcfec31b63b37d79f80471d21
Silvan Jegen <s.jegen@gmail.com>
2015-12-07 17:47:39 +0100

    Fix warnings (again)
    Conflicts:
    	src/regex_replace.c
    	src/treebank_tokenize.c

--- src/regex_replace.c
+++ src/regex_replace.c
@@ -6,8 +6,8 @@ char* regex_replace(char* pattern, char* replacement, char* string, size_t strin
 	int error_code;
 	PCRE2_SIZE error_offset;
 	uint32_t options = PCRE2_SUBSTITUTE_GLOBAL;
-	pcre2_code* regex = pcre2_compile((PCRE2_UCHAR *) pattern, pattern_length, options,
-		&error_code, &error_offset, NULL);
+	pcre2_code* regex = pcre2_compile((PCRE2_UCHAR *) pattern, 
+		pattern_length, options, &error_code, &error_offset, NULL);
 	if (regex == NULL) {
 		fprintf(stderr, "Unable to compile pattern: %s\n", pattern);
 		return NULL;
@@ -17,9 +17,9 @@ char* regex_replace(char* pattern, char* replacement, char* string, size_t strin
 	size_t substitution_length = 1000;
 	PCRE2_UCHAR* substitution = malloc(string_length);
 
-	int ret = pcre2_substitute(regex, string, string_length, start_offset,
-		options, NULL, NULL, replacement, replacement_length,
-		substitution, &substitution_length);
+	int ret = pcre2_substitute(regex,(PCRE2_SPTR8) string, string_length, 
+		start_offset, options, NULL, NULL, (PCRE2_SPTR8) replacement, 
+		replacement_length, substitution, &substitution_length);
 	if (ret < 0) {
 		fprintf(stderr, "Failed to perform substitution: %s %d.\n",
 			pattern, ret);
@@ -27,8 +27,7 @@ char* regex_replace(char* pattern, char* replacement, char* string, size_t strin
 	}
 	//char* result = strdup(&substitution);
 	//fprintf(stderr, "Found something. Pattern: %s Ret: %d input: %s resultstring: %s outlen: %d.\n", pattern, ret, string, substitution, substitution_length);
-
-	char* result = strdup(substitution);
+	char* result = strdup((const char *) substitution);
 	fprintf(stderr, "Copied result: %s.\n", result);
 	return result;
 }
--- src/treebank_tokenize.c
+++ src/treebank_tokenize.c
@@ -5,20 +5,22 @@ static char* preprocess(char* string, size_t inputlen) {
 	//starting quotes
 	string = regex_replace("^\"", "``", string, inputlen);
 	string = regex_replace("(``)", " $1 ", string, inputlen);
-	string = regex_replace("([ (\[{<])\"", "$1 `` ", string, inputlen);
+	string = regex_replace("([ (\\[{<])\"", "$1 `` ", string, inputlen);
 
 	//punctuation
 	string = regex_replace("([:,])([^\\d])", " $1 $2", string, inputlen);
 	string = regex_replace("([:,])$", " $1 ", string, inputlen);
-	string = regex_replace("\\.\\.\\.", " ... ", string, inputlen); string = regex_replace("[;@#$%&]", " $1 ", string, inputlen);
-	string = regex_replace("([^\\.])(\\.)([\\]\\)}>\"']*)\\s*$", "$1 $2$3 ", 
-		string, inputlen);
+	string = regex_replace("\\.\\.\\.", " ... ", string, inputlen); 
+	string = regex_replace("[;@#$%&]", " $1 ", string, inputlen);
+	string = regex_replace("([^\\.])(\\.)([\\]\\)}>\"']*)\\s*$", 
+		"$1 $2$3 ", string, inputlen);
 	string = regex_replace("([?!])", " $1 ", string, inputlen);
 	
 	string = regex_replace("([^'])' ", "$1 ' ", string, inputlen);
 
 	//parens, brackets, etc.
-	string = regex_replace("([\\]\\[\\(\\)\\{\\}\\<\\>])", " $1 ", string, inputlen);
+	string = regex_replace("([\\]\\[\\(\\)\\{\\}\\<\\>])", " $1 ", string, 
+		inputlen);
 	string = regex_replace("--", " -- ", string, inputlen);
 	
 	//add extra space to make things easier
@@ -32,18 +34,25 @@ static char* preprocess(char* string, size_t inputlen) {
 	string = regex_replace("\"", " '' ", string, inputlen);
 	string = regex_replace("(\\S)('')", " $1 $2 ", string, inputlen);
 	
-	string = regex_replace("([^' ])('[sS]|'[mM]|'[dD]|') ", "$1 $2 ", string, inputlen);
+	string = regex_replace("([^' ])('[sS]|'[mM]|'[dD]|') ", "$1 $2 ", 
+		string, inputlen);
 	string = regex_replace("([^' ])('ll|'LL|'re|'RE|'ve|'VE|n't|N'T) ", 
 		"$1 $2 ", string, inputlen);
 	
 	//contractions 2
-	string = regex_replace("(?i)\b(can)(not)\b", " $1 $2 ", string, inputlen);
+	string = regex_replace("(?i)\b(can)(not)\b", " $1 $2 ", string, 
+		inputlen);
 	string = regex_replace("(?i)\b(d)('ye)\b", " $1 $2 ", string, inputlen);
-	string = regex_replace("(?i)\b(gim)(me)\b", " $1 $2 ", string, inputlen);
-	string = regex_replace("(?i)\b(gon)(na)\b", " $1 $2 ", string, inputlen);
-	string = regex_replace("(?i)\b(got)(ta)\b", " $1 $2 ", string, inputlen);
-	string = regex_replace("(?i)\b(lem)(me)\b", " $1 $2 ", string, inputlen);
-	string = regex_replace("(?i)\b(mor)('n)\b", " $1 $2 ", string, inputlen);
+	string = regex_replace("(?i)\b(gim)(me)\b", " $1 $2 ", string, 
+		inputlen);
+	string = regex_replace("(?i)\b(gon)(na)\b", " $1 $2 ", string, 
+		inputlen);
+	string = regex_replace("(?i)\b(got)(ta)\b", " $1 $2 ", string, 
+		inputlen);
+	string = regex_replace("(?i)\b(lem)(me)\b", " $1 $2 ", string, 
+		inputlen);
+	string = regex_replace("(?i)\b(mor)('n)\b", " $1 $2 ", string, 
+		inputlen);
 	string = regex_replace("(?i)\b(wan)(na) ", " $1 $2 ", string, inputlen);
 	
 	//contractions 3
@@ -61,8 +70,7 @@ treebank_tokens_t* treebank_tokenize(char* sentence) {
 	char* processed_sentence = preprocess(sentence, inputlen);
 	char* delimiters = " ";
 	treebank_tokens_t* tokens = treebank_tokens_new();
-	int num_tokens = 0;
-	char* token = strsep(&processed_sentence, delimiters);
+	char* token = strsep(&procsent, delimiters);
 	while (token != NULL) {
 		if (strlen(token) > 0) treebank_tokens_append(tokens, token);
 		token = strsep(&processed_sentence, delimiters);
